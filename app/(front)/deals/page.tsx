d6f6b5cdde3dbf619296564d6fbb6b24f218cf5c
Fuma1322 <leonardfuma26@gmail.com>
2026-06-11 00:47:30 +0000

    feat: deals page route

--- app/(front)/deals/page.tsx
+++ app/(front)/deals/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ArrowRight, Flame } from "lucide-react";
-import { Button } from "@/components/ui/button";
 
 const deals = [
   {
