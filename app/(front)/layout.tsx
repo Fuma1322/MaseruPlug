84ca3e58ed418fbda6d42994ee688efa6233280c
Fuma1322 <leonardfuma26@gmail.com>
2026-09-20 04:46:28 +0000

    fix: Footer responsiveness in mobile view

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,15 +1,22 @@
-import Footer from '@/components/Frontend/Footer'
-import MobileDock from '@/components/Frontend/MobileDock'
-import Navbar from '@/components/Frontend/Navbar'
-import React, { ReactNode } from 'react'
+import Footer from '@/components/Frontend/Footer';
+import MobileDock from '@/components/Frontend/MobileDock';
+import Navbar from '@/components/Frontend/Navbar';
+import React, { ReactNode } from 'react';
 
-export default async function Layout({children}:{children:ReactNode}) {
+export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <Navbar />
-        {children}
+    <html lang="en">
+      <body>
+        <Navbar />
+
+        <main className="min-h-screen">{children}</main>
+
+        <div className="pb-12 md:pb-0">
+          <Footer />
+        </div>
+
         <MobileDock />
-      <Footer />
-    </div>
-  )
+      </body>
+    </html>
+  );
 }
